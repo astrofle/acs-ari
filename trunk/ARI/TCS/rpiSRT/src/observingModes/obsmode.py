f75f147b7bbba0b9753202a02bae38def4dbc10b
RhoOlguin@gmail.com <RhoOlguin@gmail.com@203bdf7e-7ac3-ce1b-6535-99cc17bfeca6>
2015-03-18 00:29:22 +0000

    radec2azel implementation and SRT sources

--- trunk/ARI/TCS/rpiSRT/src/observingModes/obsmode.py
+++ trunk/ARI/TCS/rpiSRT/src/observingModes/obsmode.py
@@ -13,8 +13,10 @@ class ARI_obsmodes():
 		self.site = sites.site
 		self.planets = sites.planets
 		self.stars = sites.stars
+		self.SRTsources = sites.SRTsources
 		print str(len(self.planets))+ " observable planets: " + str(self.planets.keys())
 		print str(len(self.stars))+ " observable stars: " + str(self.stars.keys())
+		print str(len(self.SRTsources))+ " observable stars: " + str(self.SRTsources.keys())
 		self.setup = 0
 
 	def find_planets(self):
