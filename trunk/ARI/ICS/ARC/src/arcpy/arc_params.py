d678fcce36b4b538aae3468b8d855cb86d3e6b34
salas.mz <salas.mz@203bdf7e-7ac3-ce1b-6535-99cc17bfeca6>
2015-07-14 15:03:14 +0000

    Minor changes to ARC API

--- trunk/ARI/ICS/ARC/src/arcpy/arc_params.py
+++ trunk/ARI/ICS/ARC/src/arcpy/arc_params.py
@@ -2,7 +2,7 @@
 
 katcp_port = 7147
 roach_ip = '146.155.121.6'
-synth_port = '/dev/valonsynth'
+synth_port = '/dev/ttyUSB0'
 # Those that have been successful tested on the AIUC ROACH board
 allowed_config = {100e6:[97656.25, 48828.125, 24414.0625],
                   400e6:[390625.0]}
